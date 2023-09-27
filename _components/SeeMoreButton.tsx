60ca9cec3b16eedd3069b24c00dda7219c671f14
Phongsavanh Mongkhonvilay <pmongkho12@gmail.com>
2023-09-27 13:08:02 -0500

    change exp and school

--- _components/SeeMoreButton.tsx
+++ _components/SeeMoreButton.tsx
@@ -6,16 +6,17 @@ export default function SeeMoreButton({ description }:any) {
  	const [seeMore, setSeeMore] = useState(false)
  
     return (
-		<div>
-			<p className={` leading-7 mb-2 ${seeMore ? '' : 'line-clamp-5'}`}>
-				{description}
-			</p>
-			<button
-				onClick={() => setSeeMore(!seeMore)}
-				className='leading-7 mb-2'
-			>
-				{seeMore ? 'see less' : 'see more'}
-			</button>
-		</div>
-	)
+			<div>
+				<p className={` leading-7 mb-2 ${seeMore ? '' : 'line-clamp-5'}`}>
+					<ul className=' list-disc'>
+						{description?.map((descr: string) => (
+							<li>{descr}</li>
+						))}
+					</ul>
+				</p>
+				<button onClick={() => setSeeMore(!seeMore)} className='leading-7 mb-2'>
+					{seeMore ? 'see less' : 'see more'}
+				</button>
+			</div>
+		)
 }
