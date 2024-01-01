107bfd8da8d11998b6f17a6a96eb6dfa9a6e996c
Phongsavanh Mongkhonvilay <pmongkho12@gmail.com>
2023-12-31 20:46:31 -0600

    added coolerheater

--- _components/Projects.tsx
+++ _components/Projects.tsx
@@ -2,12 +2,60 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import inkland from '../_media/inkland.png'
+import heatercooler from '../_media/heatercooler.png'
 import parkez from '../_media/park-ez.png'
 
 export default function Projects() {
 	return (
 		<div className=' text-slate-400'>
 			<p className=' heading'>PROJECTS</p>
+			<div className='flex flex-wrap-reverse w-full items-end justify-between mb-8'>
+				<Link href='https://coolerheater.azurewebsites.net/' target='_blank'>
+					<div className='lg:max-w-[12vw] '>
+						<Image
+							src={heatercooler}
+							width={250}
+							height={250}
+							alt='project picture'
+							className=' border rounded border-cyan-700 mb-8 hover:border-2 hover:shadow-xl transition '
+						/>
+					</div>
+				</Link>
+				<div className='lg:max-w-[25vw] md:max-w-[50vw] '>
+					<Link href='https://inkland.vercel.app/' target='_blank'>
+						<p className=' mb-4 text-white text-lg flex items-center hover:text-cyan-500 transition '>
+							CoolerHeater
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								fill='none'
+								viewBox='0 0 24 24'
+								strokeWidth={1.5}
+								stroke='currentColor'
+								className='w-4 h-4 mx-2 '
+							>
+								<path
+									strokeLinecap='round'
+									strokeLinejoin='round'
+									d='M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25'
+								/>
+							</svg>
+						</p>
+					</Link>
+					<p>
+						CoolerHeater is an app that requests a drone that provides the
+						service of heating and cooling clients with a push of a button.
+					</p>
+					<div className=' flex flex-wrap py-4'>
+						<span className='technology-item'>C#</span>
+						<span className='technology-item'>ASP.NET</span>
+						<span className='technology-item'>MongoDB</span>
+						<span className='technology-item'>Mapbox API</span>
+						<span className='technology-item'>Stripe API</span>
+						<span className='technology-item'>Angular</span>
+						<span className='technology-item'>Tailwind CSS</span>
+					</div>
+				</div>
+			</div>
 			<div className='flex flex-wrap-reverse w-full items-end justify-between mb-8'>
 				<Link href='https://inkland.vercel.app/' target='_blank'>
 					<div className='lg:max-w-[12vw] '>
@@ -95,6 +143,7 @@ export default function Projects() {
 						<span className='technology-item'>Next.js</span>
 						<span className='technology-item'>Tailwind CSS</span>
 						<span className='technology-item'>MongoDB</span>
+						<span className='technology-item'>Stripe API</span>
 					</div>
 				</div>
 			</div>
