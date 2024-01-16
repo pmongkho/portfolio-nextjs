feddb2fa86863cf3023df14d165dcce0ff728398
Phongsavanh Mongkhonvilay <pmongkho12@gmail.com>
2024-01-15 19:15:30 -0600

    update 01-15-24

--- _components/SeeMoreButton.tsx
+++ _components/SeeMoreButton.tsx
@@ -6,18 +6,15 @@ export default function SeeMoreButton({ description }:any) {
  	const [seeMore, setSeeMore] = useState(false)
  
     return (
-		<div>
-			<p className={` leading-7 mb-2 ${seeMore ? '' : 'line-clamp-5'}`}>
-				{description.map((descr:any) => (
-					<p>{descr}</p>
-				))}
-			</p>
-			<button
-				onClick={() => setSeeMore(!seeMore)}
-				className='leading-7 mb-2'
-			>
-				{seeMore ? 'see less' : 'see more'}
-			</button>
-		</div>
-	)
+			<div>
+				<p className={` leading-7 mb-2 ${seeMore ? '' : 'line-clamp-5'}`}>
+					{description.map((descr: any) => (
+						<p>• {descr}</p>
+					))}
+				</p>
+				<button onClick={() => setSeeMore(!seeMore)} className='leading-7 mb-2'>
+					{seeMore ? 'see less' : 'see more'}
+				</button>
+			</div>
+		)
 }
